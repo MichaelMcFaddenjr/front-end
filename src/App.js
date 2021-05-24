aa34f9efe66a50d8856a97440032fbd346c079e8
davedwight <dpdwight@gmail.com>
2021-05-24 17:10:13 -0600

    Moved state from App.js to SignUp component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute';
-import axios from 'axios';
-import * as yup from 'yup';
-import schema from './Validation/signUpFormSchema';
 
 import NavBar from './Components/NavBar';
 import Login from './Components/Login';
@@ -16,69 +13,7 @@ import UserProfile from './Components/UserProfile';
 
 import './App.css';
 
-const initialSignUpFormValues = {
-  username: '',
-  email: '',
-  phone: '',
-};
-
-const initialSignUpFormErrors = {
-  username: '',
-  email: '',
-  phone: '',
-};
-
-const initialUsers = [];
-
-const initialDisabled = false;
-
 function App() {
-
-  const [users, setUsers] = useState(initialUsers);
-  const [signUpFormValues, setSignUpFormValues] = useState(initialSignUpFormValues);
-  const [signUpFormErrors, setSignUpFormErrors] = useState(initialSignUpFormErrors);
-  const [disabled, setDisabled] = useState(initialDisabled)
-
-  const postNewUser = newUser => {
-    axios.post('https://reqres.in/api/user', newUser)
-      .then(res => {
-        setUsers([...users, res.data])
-        console.log('Post response: ', res.data)
-      })
-      .catch(err => {
-        console.log('Error: ', err)
-        return "There was an error..."
-      })
-      .finally(setSignUpFormValues(initialSignUpFormValues))
-  }
-
-  const validate = (name, value) => {
-    yup.reach(schema, name)
-      .validate(value)
-      .then(() => setSignUpFormErrors({ ...signUpFormErrors, [name]: '' }))
-      .catch(err => setSignUpFormErrors({ ...signUpFormErrors, [name]: err.errors[0] }))
-  }
-
-  const inputChange = (name, value) => {
-    validate(name, value)
-    setSignUpFormValues({
-      ...signUpFormValues, [name]: value
-    })
-  }
-
-  const signUpFormSubmit = () => {
-    const newUser = {
-      username: signUpFormValues.username.trim(),
-      email: signUpFormValues.email.trim(),
-      phone: signUpFormValues.phone.trim(),
-    }
-    postNewUser(newUser)
-  }
-
-  useEffect(() => {
-    schema.isValid(signUpFormValues).then(valid => setDisabled(!valid))
-  }, [signUpFormValues])
-
   return (
     <div className="App">
       <NavBar />
@@ -88,13 +23,7 @@ function App() {
           <Login />
         </Route>
         <Route path='/signup'>
-          <SignUp 
-            values={signUpFormValues}
-            change={inputChange}
-            disabled={disabled}
-            submit={signUpFormSubmit}
-            errors={signUpFormErrors} 
-            />
+          <SignUp />
         </Route>
         <PrivateRoute path='/myplants' component={MyPlants} />
         <PrivateRoute path='/addplant' component={AddPlant} />
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,52 +1,120 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import * as yup from 'yup';
+import schema from '../Validation/signUpFormSchema';
+import { useStore } from 'react-redux';
 
 //this is a simple form unit 2 can build  
-//only need a username, phone number and email  
+//only need a username, phone number and password  
 //add validation  
+
+const initialSignUpFormValues = {
+  username: '',
+  password: '',
+  phone_number: '',
+};
+
+const initialSignUpFormErrors = {
+  username: '',
+  password: '',
+  phone_number: '',
+};
+
+const initialDisabled = false;
 
 const SignUp = (props) => {
 
-  const { values, change, disabled, submit, errors } = props;
+  // const { values, change, disabled, submit, errors } = props;
 
-  const onChange = event => {
-    const { name, value } = event.target;
-    change(name, value);
-}
+  const [user, setUser] = useState({});
+  const [signUpFormValues, setSignUpFormValues] = useState(initialSignUpFormValues);
+  const [signUpFormErrors, setSignUpFormErrors] = useState(initialSignUpFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled)
 
-  const onSubmit = event => {
-    event.preventDefault();
-    submit();
+  const postNewUser = newUser => {
+    axios.post('https://ft-water-my-plants-3.herokuapp.com/api/users/register', newUser)
+      .then(res => {
+        setUser(res.data)
+        console.log('Post response: ', res.data)
+      })
+      .catch(err => {
+        console.log('Error: ', err)
+        return "There was an error..."
+      })
   }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setSignUpFormErrors({ ...signUpFormErrors, [name]: '' }))
+      .catch(err => setSignUpFormErrors({ ...signUpFormErrors, [name]: err.errors[0] }))
+  }
+
+  const inputChange = (e) => {
+    // validate(name, value)
+    // debugger;
+    console.log('event target', e.target);
+    setUser({
+      ...user, [e.target.name]: e.target.value
+    })
+  }
+
+  const signUpFormSubmit = () => {
+    const newUser = {
+      username: user.username.trim(),
+      password: user.password.trim(),
+      phone: user.phone_number.trim(),
+    }
+    postNewUser(newUser)
+  }
+
+  // useEffect(() => {
+  //   schema.isValid(signUpFormValues).then(valid => setDisabled(!valid))
+  // }, [signUpFormValues])
+
+
+//   const handleChange = event => {
+//     const { name, value } = event.target;
+//     change(name, value);
+// }
+
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   submit();
+  // }
+
+  const { username, password, phone } = user;
+  const { usernameError, passwordError, phoneError } = signUpFormErrors;
 
   return (
     <div>
       <h1>Plant Card</h1>
-      <form id='sign-up-form' onSubmit={onSubmit}>
+      <form id='sign-up-form' onSubmit={signUpFormSubmit}>
         <input 
-          type='name'
+          type='text'
           name='username'
           placeholder='Enter username'
-          value={values.username}
-          onChange={onChange}
+          value={username}
+          onChange={inputChange}
         />
-          <input
-            type='email'
-            name='email'
-            placeholder='Enter email'
-            value={values.email}
-            onChange={onChange}
-          />
         <input
-          type='phone'
-          name='phone'
-          placeholder='Enter phone number'
-          value={values.phone}
-          onChange={onChange}
+          type='password'
+          name='password'
+          placeholder='Enter new password'
+          value={password}
+          onChange={inputChange}
         />
-        <button disabled={disabled} id='sign-up-button'>Sign Up</button>
-        <div className='form-errors'>{errors.username}</div>
-        <div className='form-errors'>{errors.email}</div>
-        <div className='form-errors'>{errors.phone}</div>
+        <input
+          type='text'
+          name='phone_number'
+          placeholder='Enter phone number'
+          value={phone}
+          onChange={inputChange}
+        />
+        <button onClick={signUpFormSubmit} disabled={disabled} id='sign-up-button'>Sign Up</button>
+        <div className='form-errors'>{usernameError}</div>
+        <div className='form-errors'>{passwordError}</div>
+        <div className='form-errors'>{phoneError}</div>
       </form>
     </div>
   );
