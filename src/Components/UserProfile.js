56fbbebf0bed5cd06abca341aac8537c135e4599
bukit3point0 <bukit3point0@gmail.com>
2021-05-25 14:00:39 -0400

    userprofile

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 //this component is where a user can view and edit their profile info 
 //Their username and phone number should be visible but they should have the ability to edit all including their password
 const UserProfile = (props) => {
-
 const {userName, password, phone } = props()
-
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [edit, setEdit] = useState(false);
     const [values, setValues] = useState({
@@ -19,36 +17,35 @@ const {userName, password, phone } = props()
       setValues({ ...values, [e.target.name]: e.target.value });
     };
   
-    // useEffect(()=>{
-    //   axiosWithAuth()
-    //   .get(`/users/${user_id}`)
-    //   .then(res=>{
-    //     setValues(res.data)
-    //   })
-    //   .catch(err=>{
-    //     console.log(err);
-    //   })
-    // }, []);
-//
-    // const handleSubmit = (e) => {
-    //   e.preventDefault();
-    //   setShowSuccessMessage(true);
-    //   axiosWithAuth()
-    //   .put(`/users/${user_id}`, values)
-    //   .then(res=>{
-    //     setValues(res.data);
-    //     push(`/users/${user_id}`);
-    //   })
-    //   .catch(err=>{
-    //     console.log(err);
-    //   })
-    // };
+    useEffect(()=>{
+      axiosWithAuth()
+      .get(`/users/${user_id}`)
+      .then(res=>{
+        setValues(res.data)
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }, []);
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setShowSuccessMessage(true);
+      axiosWithAuth()
+      .put(`/users/${user_id}`, values)
+      .then(res=>{
+        setValues(res.data);
+        push(`/users/${user_id}`);
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    };
     
     return (
     <>
-      {/* <form onSubmit={handleSubmit}> */}
+      <form onSubmit={handleSubmit}>
         <h2>Update Profile Form:</h2>
-        {/* <label>
+        <label>
           Password:
           <input
             name='password'
@@ -67,7 +64,6 @@ const {userName, password, phone } = props()
         </label>
         <button onClick={handleSubmit}>Submit</button>
       </form>
-
       {showSuccessMessage && (
         <div className="success-message" data-testid="successMessage">
           <p>
@@ -80,9 +76,8 @@ const {userName, password, phone } = props()
             {values.phone}
           </p>
         </div>
-      )} */}
+      )}
     </>
   );
 };
-
 export default UserProfile;
