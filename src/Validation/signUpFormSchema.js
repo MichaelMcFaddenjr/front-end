072b6157507cf800d3fb0be277a27eb603113d35
davedwight <dpdwight@gmail.com>
2021-05-25 11:16:54 -0600

    Fixed bug on submit and rewired validation

--- src/Validation/signUpFormSchema.js
+++ src/Validation/signUpFormSchema.js
@@ -1,18 +1,19 @@
 import * as yup from 'yup';
 
 const phoneRegex = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const passRegex = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
 
 const formSchema = yup.object().shape({
     username: yup
         .string()
         .trim()
+        .min(3, 'Username must be at least three characters')
         .required('Username is required'),
-    email: yup
+    password: yup
         .string()
-        .trim()
-        .email('Please enter a valid email address')
-        .required('Email is required'),
-    phone: yup
+        .matches(passRegex, 'Password must contain at least 8 characters, one uppercase, one number and one special case character')
+        .required('Password is required'),
+    phone_number: yup
         .string()
         .trim()
         .matches(phoneRegex, 'Please enter a valid phone number')
